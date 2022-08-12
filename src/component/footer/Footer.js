cfaafe95b78c71cc63f662727aba8081f4334c23
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-08-13 00:42:19 +0530

    Add footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { Button, Col, Container, Row} from "react-bootstrap";
+import { Button, Col, Container, Row, Figure} from "react-bootstrap";
 import '../../styles/apply/apply.css'
+import sb_logo from "../../assets/images/sb_logo.png";
+import wielogo from "../../assets/images/wielogo.png";
+import uwulogo from "../../assets/images/uwu.png";
+import * as Icon from 'react-bootstrap-icons';
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <Container fluid className="section footer">
-      <p>SHECODERess 2022 - All Rights reserved.</p>
+      <Row >
+      <Col lg={6} md={6} sm={12} xs={12}>
+    <h2 className="topic">General Information</h2>
+    <p>Women in Engineering Affinity Group of IEEE <br/>
+        Uva Wellassa University of Sri Lanka
+    </p>
+    <Col lg={12} md={6} sm={12} xs={12}>
+                    <Figure.Image width={180} height={180} alt="171x180" src={sb_logo} />
+                    <Figure.Image width={80} height={140} alt="171x180" src={wielogo}/>
+                    <Figure.Image width={90} height={140} alt="171x180" src={uwulogo}/>
+                </Col>
+</Col>
+<Col lg={6} md={6} sm={12} xs={12}>
+<h2 className="topic">Contatc us</h2>
+    <p>
+        phone : +94712223619<br/>
+        email : ieeewieuwu@gmail.com
+    </p>
+    <Icon.Facebook style={{marginRight:"10px"}}/>
+    <Icon.Linkedin />
+</Col>
+        <Col lg={12} md={12} sm={12} xs={12} className="topic-content apply-div">
+        <p><br/><br/>SHECODERess 2022 - All Rights reserved.</p>   
+        </Col>
+      </Row>
     </Container>
   );
 };
--- src/component/gallery/Gallery.js
+++ src/component/gallery/Gallery.js
@@ -1,21 +1,50 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import ImageGallery from 'react-image-gallery';
- 
+import slider1 from "../../assets/images/slider/slider1.jpg";
+import slider2 from "../../assets/images/slider/slider2.jpg";
+import slider4 from "../../assets/images/slider/slider4.jpg";
+import slider5 from "../../assets/images/slider/slider5.jpg";
+import slider6 from "../../assets/images/slider/slider6.jpg";
+import slider7 from "../../assets/images/slider/slider7.jpg";
+import slider8 from "../../assets/images/slider/slider8.jpg";
+import slider9 from "../../assets/images/slider/slider9.jpg";
+
+
 export const Gallery = () => {
    const images = [
        {
-         original: 'https://picsum.photos/id/1018/1000/600/',
-         thumbnail: 'https://picsum.photos/id/1018/250/150/',
+         original: slider1,
+         thumbnail: slider1,
        },
        {
-         original: 'https://picsum.photos/id/1015/1000/600/',
-         thumbnail: 'https://picsum.photos/id/1015/250/150/',
-       },
-       {
-         original: 'https://picsum.photos/id/1019/1000/600/',
-         thumbnail: 'https://picsum.photos/id/1019/250/150/',
-       },
+        original: slider2,
+        thumbnail: slider2,
+      },
+      {
+        original: slider4,
+        thumbnail: slider4,
+      },
+      {
+        original: slider5,
+        thumbnail: slider5,
+      },
+      {
+        original: slider6,
+        thumbnail: slider6,
+      },
+      {
+        original: slider7,
+        thumbnail: slider7,
+      },
+      {
+        original: slider8,
+        thumbnail: slider8,
+      },
+      {
+        original: slider9,
+        thumbnail: slider9,
+      },
      ];
  return (
    <Container fluid className="section gallery">
