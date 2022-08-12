10566dc8519a42447bd12e19014b51116fef38b6
Abitharani Jeyachandran <abitharani009@gmail.com>
2022-08-12 13:49:09 +0530

    Change prize section

--- src/component/prize/Prize.js
+++ src/component/prize/Prize.js
@@ -8,7 +8,7 @@ export const Prize = () => {
     <h1 className="topic" data-aos="fade-up">Prizes</h1>  
     <Row >
         <Col lg={12} md={12} sm={12} xs={12} className="topic-content" data-aos='fade-up' data-aos-delay="100"> 
-        <Figure.Image alt="171x180" width="60%" src={background}/>
+        <Figure.Image alt="171x180" width="80%" src={background}/>
           </Col>
       </Row>   
     </Container>
