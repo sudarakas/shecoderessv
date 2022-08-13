4c724f16b2b14e4d8b3a69fd24166af635860ccc
wimanshaherath <wimanshah@gmail.com>
2022-08-13 15:52:46 +0530

    change reg closed date

--- src/component/timeline/index.js
+++ src/component/timeline/index.js
@@ -4,7 +4,7 @@ import { Timeline } from "./Timeline";
 const Index = () => {
   const [content, setContent] = useState([
     { text: "Registration Open", date: "12th Auguest, 2022" },
-    { text: "Registration Closed", date: "19th Auguest, 2022" },
+    { text: "Registration Closed", date: "16th Auguest, 2022" },
     { text: "Technical Session", date: "" },
     { text: "Hackathon day", date: "2nd September, 2022" },   
   ]);
